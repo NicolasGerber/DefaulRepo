8126445a2e92a87e9c274b134e552ce08b924267
gerberzitos <nicolasgergerber@gmail.com>
2024-09-05 16:00:28 -0300

    Alteração (Adicionei o codigo em C para comparação)

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ ant = num - 1
 print(f"Você digitou: {num}")
 print(f"O próximo número é: {prox}")
 print(f"O número anterior é: {ant}")
+
+#Versão C
+# #include <stdio.h>
+#int main()
+#{
+#int num, prox, ant;
+#      printf("Digite um numero:");
+#      scanf("%d", &num);
+#      prox = num + 1;
+#      ant = num - 1;
+#     printf("Voce digitou: %d\n",num);
+#     printf("O proximo numero e: %d\n",prox);
+#    printf("O numero anterior e: %d\n", ant);
+#   return (0);
+#     }
