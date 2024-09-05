88a927c5e63daadb48b7400b2f29174e98d98e4f
gerberzitos <nicolasgergerber@gmail.com>
2024-09-05 15:52:24 -0300

    Exercicio de aplicação 1 (Inserir numero e trazer o sucessor e antecessor)

---
+++ main.py
@@ -0,0 +1,11 @@
+# Solicita ao usuário para digitar um número
+num = int(input("Digite um número: "))
+
+# Calcula o próximo e o número anterior
+prox = num + 1
+ant = num - 1
+
+# Exibe os resultados
+print(f"Você digitou: {num}")
+print(f"O próximo número é: {prox}")
+print(f"O número anterior é: {ant}")
