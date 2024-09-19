c9dc70d3e8c45c1dc741969ca106995cefea41a6
gerberzitos <nicolasgergerber@gmail.com>
2024-09-19 19:02:08 -0300

    Exercicio 18(While com IF , Relatorio de satisfação)

--- main.py
+++ main.py
@@ -1,55 +1,67 @@
-#_______________EXERCICIO_17_________________________________
-#calcule e apresente a média de altura e idade das pessoas. A entrada de dados é encerrada quando for digitado o valor zero para a idade.
-t_idade = 0
-t_alt = 0
-m_idade = 0
-m_alt = 0
+#_______________EXERCICIO_18________________________________
+#Faça um programa que leia idade e opinião e apresente:
+#O número de clientes que responderam satisfatório, a média de idade dos clientes que opinaram como satisfatório.
+#E o número de clientes que responderam insatisfatório.
+# O programa se encerra quando for digitado o valor zero para idade.
 qtd=0
-idade = int(input("Digite a idade ou (0) para finalizar:"))
+t_idade = 0
+m_idade = 0
+cont = 0
+sat = 0
+insat = 0
+
+idade = int(input("Digite a idade do cliente ou (0) para finalizar:"))
 while (idade != 0):
-    alt=float(input("Digite sua altura"))
+    print(f"Como foi sua experiencia com o produto?\n")
+    opn = int(input("1 – satisfatório \n2 – indiferente \n3 – insatisfatório\n"))
     qtd = qtd + 1
-    t_alt = t_alt + alt
     t_idade = t_idade + idade
-    idade = int(input("Digite a idade ou (0) para finalizar:"))
+    if (opn == 1):
+        sat = sat +1
+    elif (opn == 3):
+        insat = insat + 1
+    idade = int(input("Digite a idade do cliente ou (0) para finalizar:"))
 m_idade = t_idade/qtd
-m_alt = t_alt/qtd
-print(f"A media de idade é: {m_idade:.2f}")
-print(f"A media de altura é: {m_alt:.2f}")
+print(f"A Quantidade de cliente que responderam como satisfatorio foi: {sat}")
+print(f"A media de idade deles é: {m_idade:.2f}")
+print(f"A Quantidade de cliente que responderam como insatisfatorio foi: {insat}")
+
 
 
 # Versão em C
-# include <stdio.h>
-# include <stdlib.h>
-# include <locale.h>
-# include <string.h>
-# int
-# main()
-# {
-#     setlocale(LC_ALL, "Portuguese");
-# int
-# idade, qtd;
-# float
-# alt, m_idade, m_alt, t_alt, t_idade;
-# t_idade = 0;
-# t_alt = 0;
-# m_idade = 0;
-# m_alt = 0;
-# qtd = 0;
-# printf("Digite a idade ou (0) para finalizar:\n");
-# scanf("%d", & idade);
-# while (idade != 0){
-# printf("Digite sua altura:");
-# scanf("%f", & alt);
-# qtd++;
-# t_alt = t_alt + alt;
-# t_idade = t_idade + idade;
-# printf("Digite a idade ou (0) para finalizar:\n");
-# scanf("%d", & idade);
-# }
-# m_idade = t_idade / qtd;
-# m_alt = t_alt / qtd;
-# printf("A media de idade é: %.2f \n", m_idade);
-# printf("A media de altura é: %.2f \n", m_alt);
-# return (0);
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#  int main(){
+# 	setlocale(LC_ALL, "Portuguese");
+# 	int qtd,idade,opn,cont,t_idade,insat,sat;
+# 	float m_idade;
+# 	qtd=0;
+# 	t_idade = 0;
+# 	m_idade = 0;
+# 	cont = 0;
+# 	sat = 0;
+# 	insat = 0;
+# 	printf("Digite a idade do cliente ou (0) para finalizar:\n");
+# 	scanf("%d",&idade);
+# 	while (idade !=0){
+# 		printf("Como foi sua experiencia com o produto?\n");
+# 		printf("1 – satisfatório \n2 – indiferente \n3 – insatisfatório\n");
+# 		scanf("%d",&opn);
+# 		qtd++;
+# 		t_idade = t_idade + idade;
+# 		if (opn == 1){
+# 			sat++;
+# 		} else if (opn == 3){
+# 			insat++;
+# 		}
+# 		printf("Digite a idade do cliente ou (0) para finalizar:\n");
+# 		scanf("%d",&idade);
+# 	}
+# 	m_idade = t_idade/qtd;
+# 	printf("A Quantidade de cliente que responderam como satisfatorio foi: %d\n",sat);
+# 	printf("A media de idade deles é: %.2f\n",m_idade);
+# 	printf("A Quantidade de cliente que responderam como insatisfatorio foi: %d\n",insat);
+# 	return(0);
 # }
