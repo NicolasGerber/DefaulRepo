99c44ff8baa2c990565e9a3af7208f53f534742a
gerberzitos <nicolasgergerber@gmail.com>
2024-09-19 16:13:44 -0300

    Exercicio 14(Estrutura for)

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
-#_______________EXERCICIO_12_________________________________
-#Faça um programa que leia um número inteiro e calcule o seu fatorial.
-num=int(input("Escreva um numero inteiro positivo:"))
-fat = 1
-for i in range(1,num + 1):
-        fat *= i
-        if i == num:
-           break
-print(f"O fatorial de {num} é: {fat}")
+#_______________EXERCICIO_14_________________________________
+#Elabore um programa que apresente todos os números divisíveis por três que sejam menores que 100.
+for i in range(3,100,3):
+    print(f"{i}")
 
-# Versão em C
-# include <stdio.h>
-# include <stdlib.h>
-# include <locale.h>
-# include <string.h>
-# int
-# main()
+
+#Versão em C
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#int main(){
+# setlocale(LC_ALL, "Portuguese");
+# int i;
+# for (i=3;i<=100;i=i+3)
 # {
-#         setlocale(LC_ALL, "Portuguese");
-# int
-# num, i, fat;
-# printf("Digite um numero inteiro positivo:");
-# scanf("%d", & num);
-# fat = 1;
-# for (i=1;i <= num;i++)
-# {
-#
-#         fat = fat * i;
-#
+# 	printf("Estes são os numeros divisiveis por 3:\n");
+# 	system("pause");
+# 	printf("%d\n",i);
 # }
-# printf("O fatorial de %d é :%d", num, fat);
-# return (0);
-# }
+# return(0);
+ # }
