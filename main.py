d2518546f447785d2bb581ecfbd018e173226d45
gerberzitos <nicolasgergerber@gmail.com>
2024-09-25 16:34:21 -0300

    Exercicio 28(vetor e busca) only PY)

--- main.py
+++ main.py
@@ -1,45 +1,22 @@
-#_______________EXERCICIO_26________________________________
-#Elaborer um vetor que armazene os numeros e os ordene.
+#_______________EXERCICIO_28________________________________
+#Elaborer um vetor que armazene os numeros e os busque
+#
+
 vetorA = []
 for i in range(10):
     elemento =int(input(f"Digite o {i+1}ª elemento do vetor:"))
     vetorA.append(elemento)
 
-    for i in range (len(vetorA) - 1):
-        for j in range(i+1, len(vetorA)):
-            if vetorA[i] > vetorA[j]:
-                vetorA[i], vetorA[j] = vetorA[j], vetorA[i]
-    print("\nVETOR ORDENADO:")
-    for elemento in vetorA:
-            print(f"{elemento} - ", end="")
-    print()
+busca = int(input("Informe o elemento que deseja buscar:"))
+posicoes = []
 
-    # Versão em C
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <string.h>
-#  int main(){
-# 	setlocale(LC_ALL, "Portuguese");
-# 	int vetorA[10];
-# 	int i,j,troca;
-# 	for (i=0;i<10;i++){
-# 		printf("Digite o %d elemento do vetor:\n",i);
-# 		scanf("%d",&vetorA[i]);
-# 		}
-# 	for (i=0;i<9;i++){
-# 		for (j=i+1;j<10;j++){
-# 			if(vetorA[i] > vetorA[j]){
-# 				troca = vetorA[i];
-# 				vetorA[i] = vetorA[j];
-# 				vetorA[j] = troca;
-# 			}
-# 		}
-# 	}
-# 	printf("\n VETOR ORDENADO:\n");
-# 	for (i=0;i<10;i++)
-# 	{
-# 		printf("%d - ", vetorA[i]);
-# 	}
-# 	return(0);
-# }
+for i in range(len(vetorA)):
+    if vetorA[i] == busca:
+        posicoes.append(i+1)
+
+if posicoes:
+    print(f"O elemento {busca} foi encontrado nas posições: {', '.join(map(str, posicoes))}.")
+else:
+    print("O elemento não foi localizado.")
+
+
