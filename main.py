0d280c15601265423c68ceb6caec8e3bb13d3fde
gerberzitos <nicolasgergerber@gmail.com>
2024-09-24 22:02:53 -0300

    Exercicio 26 (area total de uma casa)

--- main.py
+++ main.py
@@ -1,94 +1,49 @@
-#_______________EXERCICIO_25________________________________
-#Elabore um programa que receba a idade, o peso, o sexo e o estado civil de várias
-# pessoas e imprima a quantidade daquelas que são casadas, solteiras, separadas
-# e viúvas. Apresente a média de idade e de peso. O algoritmo finaliza quando for
-# informado o valor zero para idade.
-total_i = 0
-media_i = 0
-total_p = 0
-media_p = 0
-qtd = 0
-casada = 0
-solteira = 0
-divorciada = 0
-viuva = 0
-idade = int(input("Digite a idade:"))
-while (idade != 0):
-    qtd = qtd + 1;
-    total_i = total_i + idade
-    peso = float(input("Digite o peso:"))
-    total_p = total_p + peso
-    sexo = (input("Digite o sexo:"))
-    print("Digite o estado civil:")
-    estado_cv = int(input("1 - Casado/a \n 2 - Solteiro/a \n 3 - Divorciado/a \n4 - Viuvo/a \n"))
-    if (estado_cv == 1):
-        casada = casada + 1
-    elif (estado_cv == 2):
-        solteira = solteira + 1
-    elif (estado_cv == 3):
-        divorciada = divorciada + 1
-    elif (estado_cv == 4):
-        viuva = viuva + 1
-    idade = int(input("Digite a idade:"))
-media_i = total_i/qtd
-media_p = total_p/qtd
-print(f"No total são:\n")
-print(f"{casada} pessoas casadas\n")
-print(f"{solteira} pessoas solteiras\n")
-print(f"{divorciada} pessoas divorciadas\n")
-print(f"{viuva} pessoas viuvas\n")
-print(f"{media_i:.2f} média de idade é\n")
-print(f"{media_p:.2f} média de peso é\n")
-
+#_______________EXERCICIO_26________________________________
+# Construa um programa que possibilite calcular a área total de uma residência (sala,
+# cozinha, banheiro, quartos etc.). O programa deve solicitar a entrada do nome, a
+# largura e o comprimento de determinado cômodo até que o nome do cômodo seja
+# “FIM”. O programa deve apresentar o valor total acumulado da área residencial.
+area = 0
+total = 0
+nome = (input("Escreva o nome do comodo ou (FIM) para finalizar:"))
+while (nome.lower() != "fim"):
+    largura = float(input("Escreva a largura do comodo:"))
+    comprimento = float(input("Escreva o comprimento do comodo:"))
+    area = largura * comprimento
+    total += area
+    nome = (input("Escreva o nome do comodo ou (FIM) para finalizar:"))
+print(f"A area total da case é: {total:.2f}")
 
 # Versão em C
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <string.h>
-# int main(){
-# 	setlocale(LC_ALL, "Portuguese");
-# 	int qtd,idade,estado_cv,casada,solteira,divorciada,viuva;
-# 	float peso,media_i,total_i,media_p,total_p;
-# 	char sexo[1];
-# 	total_i = 0;
-# 	media_i = 0;
-# 	total_p = 0;
-# 	media_p = 0;
-# 	printf("Digite a idade:\n");
-# 	scanf("%d",&idade);
-# 	while (idade != 0){
-# 		qtd++;
-# 		total_i = total_i + idade;
-# 		printf("Digite o peso:\n");
-# 		scanf("%f",&peso);
-# 		total_p = total_p + peso;
-# 		printf("Digite o sexo");
-# 		scanf("%c",&sexo);
-# 		printf("Digite o estado civil:\n");
-# 		printf("1 - Casado/a \n 2 - Solteiro/a \n 3 - Divorciado/a \n 4 - Viuvo/a \n");
-# 		scanf("%d", &estado_cv);
-# 		switch (estado_cv){
-# 			case 1: casada++;
-# 			break;
-# 			case 2: solteira++;
-# 			break;
-# 			case 3: divorciada++;
-# 			break;
-# 			case 4: viuva++;
-# 			break;
-# 		}
-# 		printf("Digite a idade:\n");
-# 		scanf("%d",&idade);
-# 	}
-# 	media_i = total_i/qtd;
-# 	media_p = total_p/qtd;
-# 	printf("No total são:\n");
-# 	printf("%d pessoas casadas\n",casada);
-# 	printf("%d pessoas solteiras\n",solteira);
-# 	printf("%d pessoas divorciadas\n",divorciada);
-# 	printf("%d pessoas viuvas\n",viuva);
-# 	printf("A média de idade é:%.2f\n",media_i);
-# 	printf("A média de peso é:%.2f\n",media_p);
-# 	return(0);
+# include <stdio.h>
+# include <stdlib.h>
+# include <locale.h>
+# include <string.h>
+# int
+# main()
+# {
+#     setlocale(LC_ALL, "Portuguese");
+# float
+# largura, comprimento, area, total;
+# char
+# nome[50];
+# printf("Escreva o nome do comodo ou digite (FIM) para finalizar:");
+# fgets(nome, sizeof(nome), stdin);
+# nome[strcspn(nome, "\n")] = 0;
+# area = 0;
+# total = 0;
+# while (strcmp(nome, "fim") != 0 & & strcmp(nome, "FIM") != 0) {
+# printf("\nDigite a largura do comodo:");
+# scanf("%f", & largura);
+# printf("\nDigite o comprimento do comodo:");
+# scanf("%f", & comprimento);
+# area = largura * comprimento;
+# total = total + area;
+# printf("\nEscreva o nome do comodo ou digite (FIM) para finalizar:");
+# getchar();
+# fgets(nome, sizeof(nome), stdin);
+# nome[strcspn(nome, "\n")] = 0;;
+# }
+# printf("\nA area total da casa é: %.2f", total);
+# return (0);
 # }
