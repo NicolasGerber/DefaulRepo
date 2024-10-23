f800341e699dcc20e9fc8ba6fc513669786ea51e
gerberzitos <nicolasgergerber@gmail.com>
2024-10-23 20:41:33 -0300

    Atualização no Gerador, Adicionei uma opção de ter letras Minusculas e Maiusculas especificamente

--- Py100.py
+++ Py100.py
@@ -1,18 +1,22 @@
 import random
 
-letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letrasmai = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letrasmin = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 simbolos = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
 numeros = ['1','2','3','4','5','6','7','8','9','0']
 v_senha = []
 
 print("GERADOR AUTOMATICO DE SENHA")
-n_letras = int(input("Quantas letras deve conter em sua senha?\nResposta:"))
+n_letrasmin = int(input("Quantas letras minusculas deve conter em sua senha?\nResposta:"))
+n_letrasmai = int(input("Quantas letras maiusculas deve conter em sua senha?\nResposta:"))
 n_simbolos = int(input("Quantos simbolos deve conter em sua senha?:\nResposta:"))
 n_numeros =  int(input("Quantos simbolos deve conter em sua senha?:\nResposta:"))
 
 
-for i in range(0,n_letras):
-    v_senha.append(random.choice(letras))
+for i in range(0,n_letrasmin):
+    v_senha.append(random.choice(letrasmin))
+for i in range(0, n_letrasmai):
+     v_senha.append(random.choice(letrasmai))
 for i in range(0,n_simbolos):
     v_senha.append(random.choice(simbolos))
 for i in range(0,n_numeros):
