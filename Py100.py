52de49664a217ef6ff4c3688d3d2e5957b05a2a6
gerberzitos <nicolasgergerber@gmail.com>
2024-10-23 20:28:57 -0300

    Gerador automatico de senhas

--- Py100.py
+++ Py100.py
@@ -1,40 +1,24 @@
 import random
-maquina = random.randint(1, 3)
 
-escolha = int(input("JOKENPO!\n escolha\n 1-PEDRA\n 2-PAPEL\n 3-TESOURA\n"))
-if escolha == 1 and maquina == 1:
-    print("Você escolheu PEDRA")
-    print("A Maquina escolheu PEDRA")
-    print("EMPATE!")
-elif escolha == 1 and maquina == 2:
-    print("Você escolheu PEDRA")
-    print("A Maquina escolheu PAPEL")
-    print("PERDEU!")
-elif escolha == 1 and maquina == 3:
-    print("Você escolheu PEDRA")
-    print("A Maquina escolheu TESOURA")
-    print("GANHOU!")
-if escolha == 2 and maquina == 1:
-    print("Você escolheu PEPEL")
-    print("A Maquina escolheu PEDRA")
-    print("GANHOU!")
-elif escolha == 2 and maquina == 2:
-    print("Você escolheu PAPEL")
-    print("A Maquina escolheu PAPEL")
-    print("EMPATE!")
-elif escolha == 2 and maquina == 3:
-    print("Você escolheu PAPEL")
-    print("A Maquina escolheu TESOURA")
-    print("PERDEU!")
-if escolha == 3 and maquina == 1:
-    print("Você escolheu TESOURA")
-    print("A Maquina escolheu PEDRA")
-    print("PERDEU!")
-elif escolha == 3 and maquina == 2:
-    print("Você escolheu TESOURA")
-    print("A Maquina escolheu PAPEL")
-    print("GANHOU!")
-elif escolha == 3 and maquina == 3:
-    print("Você escolheu TESOURA")
-    print("A Maquina escolheu TESOURA")
-    print("EMPATE!")
+letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+simbolos = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+numeros = ['1','2','3','4','5','6','7','8','9','0']
+v_senha = []
+
+print("GERADOR AUTOMATICO DE SENHA")
+n_letras = int(input("Quantas letras deve conter em sua senha?\nResposta:"))
+n_simbolos = int(input("Quantos simbolos deve conter em sua senha?:\nResposta:"))
+n_numeros =  int(input("Quantos simbolos deve conter em sua senha?:\nResposta:"))
+
+
+for i in range(0,n_letras):
+    v_senha.append(random.choice(letras))
+for i in range(0,n_simbolos):
+    v_senha.append(random.choice(simbolos))
+for i in range(0,n_numeros):
+    v_senha.append(random.choice(numeros))
+random.shuffle(v_senha)
+senha=""
+for i in v_senha:
+    senha+=i
+print(f"SUA SENHA É: {senha}")
