4e0ec98aabd423ed75e56418bbceeed311a5ef9c
gerberzitos <nicolasgergerber@gmail.com>
2024-11-25 19:10:10 -0300

    File managing and replacement exercise

--- main.py
+++ main.py
@@ -1,56 +1,15 @@
-from turtle import Screen
-import time
-from snake import Snake
-from scoreboard  import Food,Scoreboard
-import pygame
-pygame.init()
+with open("../EstudosCtoPY/Input/names.txt", mode='r') as names:
+    all_names = [name.strip() for name in names.readlines()]
 
-
-screen = Screen()
-screen.setup(600,600)
-screen.bgcolor("black")
-screen.title("SNAKE GAME")
-screen.tracer(0)
-
-
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
-
-
-screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.07)
-    snake.move()
+with open("../EstudosCtoPY/Input/Letters/starting_letter.txt", mode='r') as letter:
+    content = letter.read()
+    for name in all_names:
+       new_letter =  content.replace("[name]", name)
+       with open(f"../EstudosCtoPY/Output/ReadyToSend/Letter_for_{name}.docx", mode="w") as complete_letter:
+           complete_letter.write(new_letter)
 
 
 
-    #Food Collision
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-
-    #Wall Collision
-    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        scoreboard.endgame()
-        time.sleep(1)
-        snake.reset_position()
-
-    #Tail Collision
-    for segments in snake.all_snakes:
-        if segments == snake.head:
-            pass
-        elif snake.head.distance(segments) < 10:
-            scoreboard.endgame()
-            time.sleep(1)
-            snake.reset_position()
 
 
-screen.exitonclick()
+
