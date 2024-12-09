56b7aa17a17ad8a1e2d0f7e89c5a4fb0d7a155ea
gerberzitos <nicolasgergerber@gmail.com>
2024-12-09 18:01:04 -0300

    Pomodoro timer

--- main.py
+++ main.py
@@ -1,81 +1,105 @@
-from turtle import Turtle, Screen
-import pandas as pd
-from map_text import Texto
-import time
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+# ---------------------------- TIMER RESET ------------------------------- # 
 
-data = pd.read_csv("points.csv", index_col=False)
-points = data.to_dict("list")
-points["state"] = [state.strip().title() for state in points["state"]]
-
-text = Texto()
-turtle = Turtle()
-screen = Screen()
-time_text = Turtle()
-
-screen.setup(947, 980)
-screen.title("South America Countries and Brazil States Game")
-image = "map.gif"
-screen.addshape(image)
-turtle.shape(image)
-
-start = time.time()
-time_limit = 125
-i = 0
-game_is_on = True
-guessed_answers = []
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="TIMER")
+    checkmark.config(text="")
+    global reps
+    reps = 0
 
 
-def update_time():
-    elapsed_time = int(time.time() - start)
-    remaining_time = time_limit - elapsed_time
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps +=1
+    work_sec = WORK_MIN *60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if reps % 2 != 0:
+        count_down(work_sec)
+        timer_label.config(text="FOCUS",fg=GREEN)
+    elif reps % 2 == 0 and reps != 8:
+        count_down(short_break_sec)
+        timer_label.config(text="BREAK",fg=PINK)
+    elif reps == 8:
+        count_down(long_break_sec)
+        timer_label.config(text="BREAK",fg=RED)
 
 
-    time_text.clear()
-    time_text.penup()
-    time_text.hideturtle()
-    time_text.goto(220, 417)
-    time_text.write(f"Time: {remaining_time}", font=("Courier", 30, "normal"))
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
-    if remaining_time > 0:
-        screen.ontimer(update_time, 1000)
-    else:
-        game_over()
+def count_down(count):
 
-def game_over():
-    global game_is_on
-    game_is_on = False
-    text.clear()
-    text.penup()
-    text.hideturtle()
-    text.goto(0, 0)
-    text.write("TIME'S UP! Game Over!", align="center", font=("times new roman", 30, "normal"))
-
-
-
-screen.textinput("ARE YOU READY!","Press OK to start")
-screen.ontimer(update_time, 1000)
-
-while game_is_on:
-    answer = screen.textinput(f"{i}/38 Correct", "Guess a State or a Country").title().strip()
-
-    if not answer or answer in guessed_answers:
-        pass
-
-    if answer in points["state"]:
-        index = points["state"].index(answer)
-        x = points["x"][index]
-        y = points["y"][index]
-        name = points["state"][index]
-        text.create(name, x, y)
-        guessed_answers.append(answer)
-        i += 1
-
-    if i == 38:
-        text.clear()
-        text.penup()
-        text.goto(0, 50)
-        text.write("YOU WON!", align="center", font=("times new roman", 30, "normal"))
-        text.hideturtle()
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count-1)
+    elif count < 0:
+        start_timer()
+        mark = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            mark += "✔"
+        checkmark.config(text=mark)
 
 
-screen.mainloop()
+# ---------------------------- UI SETUP ------------------------------- #
+window  = Tk()
+window.title("Pomodoro")
+window.config(padx=100,pady=50,bg=YELLOW, highlightthickness=0)
+
+
+
+canvas = Canvas(width=200,height=224,bg=YELLOW)
+tomate = PhotoImage(file="tomato.png")
+canvas.create_image(102,112,image=tomate,)
+timer_text = canvas.create_text(103,130,text="00:00", fill="white",font=(FONT_NAME, 25, "bold"))
+canvas.grid(row=2,column=2)
+
+timer_label = Label(text="TIMER",
+                    fg=GREEN,
+                    highlightthickness=0,
+                    font=(FONT_NAME,36,"bold"),
+                    bg=YELLOW)
+timer_label.grid(row=0,column=2)
+
+
+start_B = Button(text="Start",
+                 command=start_timer)
+start_B.grid(row=3,column=1)
+
+
+reset_B = Button(text="Reset",
+                 command=reset_timer)
+reset_B.grid(row=3,column=3)
+
+
+checkmark = Label(text="",
+                  fg=GREEN,
+                  highlightthickness=0,
+                  bg=YELLOW,
+                  font=(FONT_NAME,33,"bold"))
+checkmark.grid(row=4,column=2)
+
+
+
+
+window.mainloop()
