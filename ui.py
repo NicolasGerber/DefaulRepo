aa23f70984c22b9bf23e8478a092e02bbf09e8cc
gerberzitos <nicolasgergerber@gmail.com>
2025-01-04 15:38:50 -0300

    Minor Fix

--- ui.py
+++ ui.py
@@ -36,7 +36,7 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
-        if self.quiz.still_has_questions()
+        if self.quiz.still_has_questions():
             self.score_label.config(text=f"Score:{self.quiz.score}")
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text =q_text)
