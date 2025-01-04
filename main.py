1686a2d69fbf51272a8217d447633d07fe313af6
gerberzitos <nicolasgergerber@gmail.com>
2025-01-04 15:32:06 -0300

    QUIZ GAME, using quiz API to sustaing the DB and still messing with the UI

--- main.py
+++ main.py
@@ -1,41 +1,22 @@
-import requests as rq
-from datetime import datetime
-import smtplib
+from quiz_brain import QuizBrain
+from data import question_data
+from question_model import Question
+from ui import QuizInterface
 
-my_email = "pythongerber@gmail.com"
-password="rsev zovr jhww otlm"
-
-MY_LAT =-31.758625
-MY_LNG =-52.305597
-
-response = rq.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data_iss = response.json()
-
-iss_latitude = float(data_iss["iss_position"]["latitude"])
-iss_longitude = float(data_iss["iss_position"]["longitude"])
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 
-parameters = {
-    "lat":MY_LAT,
-    "lng":MY_LNG,
-    "formatted":0
-}
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
 
-var = rq.get("https://api.sunrise-sunset.org/json", params=parameters)
-var.raise_for_status()
-data = var.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
-time = int(time_now.timestamp())
+while quiz.still_has_questions():
+    quiz.next_question()
 
-if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LNG - 5 <= iss_latitude <= MY_LNG + 5 :
-    if sunset > time:
-         with smtplib.SMTP("smtp.gmail.com") as connection:
-             connection.starttls()
-             connection.login(user=my_email,password=password)
-             connection.sendmail(from_addr=my_email,
-                                 to_addrs=my_email,
-                                 msg="Subject:LOOK UP\n\nLook up at the sky the ISS is passing by NOW")
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
