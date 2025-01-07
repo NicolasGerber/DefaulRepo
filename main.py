8ae4774870040089684c22049534fc921c4b21c0
gerberzitos <nicolasgergerber@gmail.com>
2025-01-07 17:27:31 -0300

    Forecast API + SMS twilio, Auto sms every time its going to rain

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from quiz_brain import QuizBrain
-from data import question_data
-from question_model import Question
-from ui import QuizInterface
+import requests as rq
+from twilio.rest import Client
+import os
 
-question_bank = []
-for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("WEATHER_API_KEY")
+account_sid = "AC77812d86d34bccafde0f51f523235923"
+auth_token = os.environ.get("AUTH_TOKEN")
+parameters = {
+    'lat':-18.030,
+    'lon':-41.119,
+    "units":"metric",
+    "cnt":4,
+    "appid":api_key,
+}
 
+response = rq.get(Endpoint, params=parameters)
+response.raise_for_status()
+weather_data = response.json()
 
-quiz = QuizBrain(question_bank)
-quiz_ui = QuizInterface(quiz)
-
-
-while quiz.still_has_questions():
-    quiz.next_question()
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="There will be rain in the next 12 Hours",
+        from_="+16014363633",
+        to="+5553981613401",
+    )
+print(message.status)
