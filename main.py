5257b888330260a6046dcf4f80ce963f3fb90f63
gerberzitos <nicolasgergerber@gmail.com>
2025-01-10 16:18:29 -0300

    API stock market and News API auto SMS, every up movimentation on Desired stock sends an SMS with percent,and news related

--- main.py
+++ main.py
@@ -1,33 +1,63 @@
 import requests as rq
 from twilio.rest import Client
 import os
+from datetime import datetime,timedelta
 
-Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = os.environ.get("WEATHER_API_KEY")
-account_sid = "AC77812d86d34bccafde0f51f523235923"
-auth_token = os.environ.get("AUTH_TOKEN")
-parameters = {
-    'lat':-18.030,
-    'lon':-41.119,
-    "units":"metric",
-    "cnt":4,
-    "appid":api_key,
+
+
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+STOCK_KEY = "GC5KG4L52FY85ZXG"
+stock_parameters = {
+    "function":"TIME_SERIES_DAILY",
+    "symbol":STOCK_NAME,
+    "apikey":STOCK_KEY,
 }
 
-response = rq.get(Endpoint, params=parameters)
-response.raise_for_status()
-weather_data = response.json()
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+NEWS_KEY="4a5191f6c487473bba48dab2816fa460"
+news_parameters={
+    "apiKey":NEWS_KEY,
+    "qInTitle": COMPANY_NAME,
+}
 
-will_rain = False
-for hour_data in weather_data["list"]:
-    condition_code = hour_data["weather"][0]["id"]
-    if int(condition_code) < 700:
-        will_rain = True
-if will_rain:
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        body="There will be rain in the next 12 Hours",
-        from_="+16014363633",
-        to="+5553981613401",
-    )
-print(message.status)
+account_sid = "AC77812d86d34bccafde0f51f523235923"
+auth_token = os.environ.get("AUTH_TOKEN")
+
+day_before = datetime.today() - timedelta(days=3)
+day_before = day_before.strftime('%Y-%m-%d')
+
+yesterday = datetime.today() - timedelta(days=2)
+yesterday = yesterday.strftime('%Y-%m-%d')
+
+stock_rq = rq.get(url=STOCK_ENDPOINT,params=stock_parameters)
+stock_data = stock_rq.json()
+
+yesterday_stock = round(float(stock_data["Time Series (Daily)"][yesterday]["4. close"]),2)
+day_before_stock = round(float(stock_data["Time Series (Daily)"][day_before]["4. close"]),2)
+
+difference = (yesterday_stock-day_before_stock)
+diff_percent = (difference/yesterday_stock) * 100
+up_down = None
+if difference >0:
+    up_down = "🔺"
+else:
+    up_down= "🔻"
+if abs(diff_percent) > 5:
+    news_response = rq.get(NEWS_ENDPOINT, params=news_parameters)
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}% \nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    for article in formatted_articles:
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body=article,
+            from_="+16014363633",
+            to="+5553981613401",
+        )
+
+
